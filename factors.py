a2142a0be15027c713f7fe193f661add324b92a2
Remi <rku16@ic.ac.uk>
2020-02-17 11:37:57 +0000

    Updating figures with new (correect) scaling factors

--- factors.py
+++ factors.py
@@ -4,18 +4,22 @@ from matplotlib import pyplot as plt
 lorenz = lambda ms: np.sqrt(1-np.square(ms))
 softmax = lambda ms: np.exp(1)-np.exp(ms)
 tanh = lambda ms: 1/np.tanh(ms) - 1/np.tanh(1)
+i_lorenz = lambda ms: np.square(1-np.sqrt(ms))
+cosh = lambda ms: 1/np.cosh(ms)
 lin = lambda ms: 1-ms
 
-xs = np.delete(np.linspace(0, 1), [0, 49])
+n = 100
+xs = np.delete(np.linspace(0, 1, num=n), [0, n-1])
 
-fs = [lorenz, softmax, tanh, lin]
-name = ["Lorenz", "Softmax", "tanh", "linear"]
-name = ["Lorenz: √(1-x^2)", "e - e^x", "tanh(x)", "1 - x"]
+fs = [lorenz, cosh, softmax, lin, i_lorenz, tanh]
+name = ["Lorenz: √(1-m^2)", "sech(m)", "e - e^m", "1 - m", "i_Lorenz: (1-√m)^2", "coth(m)"]
 
-fig, axes = plt.subplots(2, 2, figsize=(8, 8), subplot_kw={'yticks':[]})
+fig, axes = plt.subplots(2, 3, figsize=(12, 8))
 for i,ax in enumerate(axes.flat):
-    y = np.array([fs[i](x) for x in xs])
-    y = y/y.sum()
+    f = fs[i]
+    y = np.array([f(x) for x in xs])
+    if name[i] == 'e - e^m' or name[i] == 'coth(m)':
+        y /= f(xs[0])
     ax.plot(xs, y)
     ax.set_title(name[i])
     ax.set_xlabel('market share')
