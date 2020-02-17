Adding and correcting the scaling factors(v3.1)

code/main.py
@@ -48,8 +48,8 @@ if __name__ == "__main__":
   #  FOR SCALING COMPARISON
   # a_avg = []
   # a_std = []
-  # a_data = [[],[],[],[]]
-  # for k,f in enumerate(['lorenz', 'exp', 'tanh', 'linear']):
+  # a_data = [[],[],[],[],[],[]]
+  # for k,f in enumerate(['lorenz', 'cosh', 'exp', 'linear', 'i_lorenz', 'tanh']):
       # a_data[k].append(m_shares[1][-1]-m_shares[0][-1])
   #######################################
 
@@ -84,20 +84,24 @@ if __name__ == "__main__":
 
   # from matplotlib import pyplot as plt
   # c = plt.cm.RdYlGn(np.linspace(0, 1, len(a_avg[0])))
-  # fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+  # corr = ["Lorenz: √(1-m^2)", "sech(m)", "e - e^m", "1 - m", "i_Lorenz: (1-√m)^2", "coth(m)"]
+  # fig, axes = plt.subplots(2, 3, figsize=(15, 8))
   # for i,ax in enumerate(axes.flat):
-      # for j, market_share in enumerate(a_avg[i]):
-      #   ax.errorbar(list(range(len(market_share))), market_share, yerr=a_std[i][j], errorevery=50, c=c[j], label=f"Platform {j+1}")
-      # ax.set_title('Market share evaluation for Density Barabasi arrival')
-      # ax.set_xlabel('Time (t)')
-      # ax.set_ylabel('Market Share')
-      # ax.set_ylim(0, 1)
+  #     for j, market_share in enumerate(a_avg[i]):
+  #       ax.errorbar(list(range(len(market_share))), market_share, yerr=a_std[i][j], errorevery=50, c=c[j], label=f"Platform {j+1}")
+  #     ax.set_title(f'Market share evolution with {corr[i]} correction')
+  #     ax.set_xlabel('Time (t)')
+  #     ax.set_ylabel('Market Share')
+  #     ax.set_ylim(0, 1)
   # plt.legend()
+  # plt.tight_layout()
+  # plt.show()
   # FOR BOX PLOTS
-  # plt.boxplot(a_data, labels=["Lorenz: √(1-x^2)", "e - e^x", "tanh(x)", "1 - x"])
+  # plt.boxplot(a_data, labels=["Lorenz: √(1-m^2)", "sech(m)", "e - e^m", "1 - m", "i_Lorenz: (1-√m)^2", "coth(m)"])
   # plt.ylabel("Average difference in market share")
   # plt.tight_layout()
   # plt.show()
+  # exit()
   #######################################
   # FOR ∆WTL - ∆MS EVALUATION
   #   # Append the average diff between the largest and smallest platform

code/population.py
@@ -11,11 +11,13 @@ class PopulationManager():
         self.X_max = density.shape[1]
         self.Y_max = density.shape[0]
         self.m_d = np.sqrt(np.square(self.X_max) + np.square(self.Y_max))
-        # Taking int account given market shares using the lorenz factor
+        # Taking int account given market shares using the a scaled factor
         self.factor = {
             'lorenz': lambda ms: np.sqrt(1 - np.square(ms)),
-            'exp': lambda ms: np.exp(1) - np.exp(ms), 
-            'tanh': lambda ms: 1/np.tanh(ms),
+            'exp': lambda ms: (np.exp(1) - np.exp(ms)) / (np.exp(1) - 1), 
+            'tanh': lambda ms: (1/np.tanh(ms)) / (1/np.tanh(0.01)),
+            'i_lorenz': lambda ms: np.square(1-np.sqrt(ms)),
+            'cosh': lambda ms: 1/np.cosh(ms),
             'linear': lambda ms: 1 - ms
         }[factor]
         self.wtl = waiting_time_limit
@@ -36,7 +38,6 @@ class PopulationManager():
         else:
             # Factor the market shares and normalise them
             factor = self.factor(market_shares)
-            factor = factor / factor.sum()
             # Get the probability of joining each platform w.r.t. the factoring
             factor_eq = factor * market_shares + (1-factor) * distances
             return np.random.choice(indices, p=factor_eq/factor_eq.sum()), n_user