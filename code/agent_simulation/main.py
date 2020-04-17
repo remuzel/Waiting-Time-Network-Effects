9f155da2f46872e1e6c27220001c6e3d74c2b127
Remi <rku16@ic.ac.uk>
2020-04-18 00:16:32 +0100

    Reverting to base main for simulation

--- code/agent_simulation/main.py
+++ code/agent_simulation/main.py
@@ -41,65 +41,67 @@ if __name__ == "__main__":
 
     ####################################################
     # FOR HEATMAP GENERATION
-    n = 100
-    mu = np.linspace(0, 1, num=n)
-    # Setting up the values
-    delta_market_share = np.zeros((n, n))
-    delta_total = np.zeros((n, n))
-    delta_drivers = np.zeros((n, n))
-    delta_riders = np.zeros((n, n))
-    delta_inner_1 = np.zeros((n, n))
-    delta_inner_2 = np.zeros((n, n))
-    for d, mu_d in tqdm(enumerate(mu), total=n):
-        for r, mu_r in tqdm(enumerate(mu), total=n):
+    # n = 100
+    # mu = np.linspace(0, 1, num=n)
+    # # Setting up the values
+    # delta_market_share = np.zeros((n, n))
+    # delta_total = np.zeros((n, n))
+    # delta_drivers = np.zeros((n, n))
+    # delta_riders = np.zeros((n, n))
+    # delta_inner_1 = np.zeros((n, n))
+    # delta_inner_2 = np.zeros((n, n))
+    # for d, mu_d in tqdm(enumerate(mu), total=n):
+    #     for r, mu_r in tqdm(enumerate(mu), total=n):
     ####################################################
-            iter_ms = []
-            iter_r = []
-            iter_d = []
-            city = City()
-            # Run the simulation it times
-            for i in (range(args.it)):
-                sim = AgentSimulator(args.N, names, city_shape=city.density.shape,
-                                    rider_proportion=args.u, lorenz=args.c, mu_D=mu_d, mu_R=mu_r)
-                # Sort the returned shares and agent numbers (who the winner is doesn't matter)
-                m_shares = sorted(sim.run().get_market_shares(), key=lambda x: x[-1])
-                riders = sorted(sim.get_riders(), key=lambda x: x[-1])
-                drivers = sorted(sim.get_drivers(), key=lambda x: x[-1])
-                # Store the data
-                iter_ms.append(m_shares)
-                iter_r.append(riders)
-                iter_d.append(drivers)
-            # Get means of winner / looser over the runs
-            _format = lambda d, i: np.array([conf_interval(np.array(p), axis=0)[i] for p in zip(*d)])
-            avg_ms, avg_r, avg_d = _format(iter_ms, 0), _format(iter_r, 0), _format(iter_d, 0)
-            std_ms, std_r, std_d = _format(iter_ms, 1), _format(iter_r, 1), _format(iter_d, 1)
-            data = {
-                "avg_ms": avg_ms,
-                "std_ms": std_ms,
-                "avg_r": avg_r,
-                "std_r": std_r,
-                "avg_d": avg_d,
-                "std_d": std_d
-            }
-            delta_total[d, r] = np.abs((avg_r[0,-1] + avg_d[0,-1]) - (avg_r[1,-1] + avg_d[1,-1]))
-            delta_riders[d, r] = np.abs(avg_r[0,-1] - avg_r[1,-1])
-            delta_drivers[d, r] = np.abs(avg_d[0,-1] - avg_d[1,-1])
-            delta_market_share[d, r] = np.abs(avg_ms[0,-1] - avg_ms[1,-1])
-            delta_inner_1[d, r] = avg_r[0,-1] - avg_d[0,-1]
-            delta_inner_2[d, r] = avg_r[1,-1] - avg_d[1,-1]
+    iter_ms = []
+    iter_r = []
+    iter_d = []
+    city = City()
+    # Run the simulation it times
+    for i in tqdm(range(args.it)):
+        sim = AgentSimulator(args.N, names, city_shape=city.density.shape,
+                            rider_proportion=args.u, lorenz=args.c, mu_D=args.mu_d, mu_R=args.mu_r)
+        # Sort the returned shares and agent numbers (who the winner is doesn't matter)
+        m_shares = sorted(sim.run().get_market_shares(), key=lambda x: x[-1])
+        riders = sorted(sim.get_riders(), key=lambda x: x[-1])
+        drivers = sorted(sim.get_drivers(), key=lambda x: x[-1])
+        # Store the data
+        iter_ms.append(m_shares)
+        iter_r.append(riders)
+        iter_d.append(drivers)
+    # Get means of winner / looser over the runs
+    _format = lambda d, i: np.array([conf_interval(np.array(p), axis=0)[i] for p in zip(*d)])
+    avg_ms, avg_r, avg_d = _format(iter_ms, 0), _format(iter_r, 0), _format(iter_d, 0)
+    std_ms, std_r, std_d = _format(iter_ms, 1), _format(iter_r, 1), _format(iter_d, 1)
+    data = {
+        "avg_ms": avg_ms,
+        "std_ms": std_ms,
+        "avg_r": avg_r,
+        "std_r": std_r,
+        "avg_d": avg_d,
+        "std_d": std_d
+    }
+    ####################################################
+            # delta_total[d, r] = np.abs((avg_r[0,-1] + avg_d[0,-1]) - (avg_r[1,-1] + avg_d[1,-1]))
+            # delta_riders[d, r] = np.abs(avg_r[0,-1] - avg_r[1,-1])
+            # delta_drivers[d, r] = np.abs(avg_d[0,-1] - avg_d[1,-1])
+            # delta_market_share[d, r] = np.abs(avg_ms[0,-1] - avg_ms[1,-1])
+            # delta_inner_1[d, r] = avg_r[0,-1] - avg_d[0,-1]
+            # delta_inner_2[d, r] = avg_r[1,-1] - avg_d[1,-1]
+    ####################################################
         
     # # Plot the res////ults
-    # plot_market_share(data, "agent", filename=args.plt)
+    plot_market_share(data, "agent", filename=args.plt)
 
     # Plot the heatmaps
-    data = {
-        'delta_t': delta_total,
-        'delta_r': delta_riders,
-        'delta_d': delta_drivers,
-        'delta_ms': delta_market_share,
-        'delta_i1': delta_inner_1,
-        'delta_i2': delta_inner_2
-    }
-    plot_heatmaps(data)
-    plot_heatmaps(data, n=50)
-    plot_heatmaps(data, n=75)
+    # data = {
+    #     'delta_t': delta_total,
+    #     'delta_r': delta_riders,
+    #     'delta_d': delta_drivers,
+    #     'delta_ms': delta_market_share,
+    #     'delta_i1': delta_inner_1,
+    #     'delta_i2': delta_inner_2
+    # }
+    # plot_heatmaps(data)
+    # plot_heatmaps(data, n=50)
+    # plot_heatmaps(data, n=75)
