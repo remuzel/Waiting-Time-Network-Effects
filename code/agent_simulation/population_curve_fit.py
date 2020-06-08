465cbbd78d232b4884ce1af74c563baaaa38fae4
Remi <rku16@ic.ac.uk>
2020-06-08 23:57:35 +0200

    Updating population curve fit to fit on lyft data

--- code/agent_simulation/population_curve_fit.py
+++ code/agent_simulation/population_curve_fit.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     parser.add_argument('--fixed', help='Which of the mu_waiting parameters to fix', type=int, default=0)
     args = parser.parse_args()
 
-    names = ['Uber', 'Lyft', 'Other']
+    names = ['Lyft']
 
     # Extrapolate from given market-share values
     N = 1889
@@ -34,6 +34,11 @@ if __name__ == "__main__":
         midpoint_interpolation(data[1], N-95)[65:] + [data[1,-1]],
         midpoint_interpolation(data[2], N-95)[65:] + [data[2,-1]]
     ]
+
+    # Extrapolate from the Lyft quarterly values
+    N = 961
+    data_riders = midpoint_interpolation([3.5, 4.5, 5.7, 6.6, 8.1, 9.4, 11.4, 12.6, 14, 15.5, 17.4, 18.6, 20.5, 21.8, 22.314, 22.9], N)
+    data_drivers = midpoint_interpolation([0.31, 0.44, 0.57, 0.7, 0.84, 0.98, 1.12, 1.26, 1.4, 1.52, 1.64, 1.76, 1.88, 2.01, 2.14, 2.27], N)
 
     # Define the parameters to search for
     mu_rs = np.linspace(0, 1, num=args.mu_waiting)
@@ -51,7 +56,8 @@ if __name__ == "__main__":
         if mu_d[0] != mu_ds[args.fixed]:
             continue
         try:
-            iter_users = []
+            iter_riders = []
+            iter_drivers = []
             city = City()
             # Run the simulation it times
             for _ in (range(args.it)):
@@ -60,35 +66,38 @@ if __name__ == "__main__":
                                     n_joins=1, delays=[0, 65, 65])
                 # Store the returned data
 
-                iter_users.append(sim.run().get_riders() + sim.get_drivers())
+                iter_riders.append(sim.run().get_riders())
+                iter_drivers.append(sim.get_drivers())
             # Get means of winner / looser over the runs
-            avg_users = np.array([conf_interval(np.array(p), axis=0)[0] for p in zip(*iter_users)])
-            for i, (pred, true) in enumerate(zip(avg_users, data_rides)):
-                a, b = min(pred), max(pred)
-                c, d = min(true), max(true)
-                avg_users[i] = (((pred - a) / (b-a)) * (d-c)) + c
+            avg_riders = np.array([conf_interval(np.array(p), axis=0)[0] for p in zip(*iter_riders)])
+            avg_drivers = np.array([conf_interval(np.array(p), axis=0)[0] for p in zip(*iter_drivers)])
+            
+            a, b = min(avg_riders), max(avg_riders)
+            c, d = min(data_riders), max(data_riders)
+            avg_drivers = (((avg_riders - a) / (b-a)) * (d-c)) + c
+
+            a, b = min(avg_drivers), max(avg_drivers)
+            c, d = min(data_drivers), max(data_drivers)
+            avg_drivers = (((avg_drivers - a) / (b-a)) * (d-c)) + c
 
             # Compute RMSE for half of the data
-            rmse = []
-            for true_ms, pred_ms in zip(data_rides, avg_users):
-                a = int(len(true_ms) * 0.50)
-                true = true_ms[:a]
-                pred = pred_ms[:a]
-                rmse.append(np.sqrt(mean_squared_error(true, pred)))
+            a = int(len(data_riders) * 0.7)
+            rmse = np.sqrt(mean_squared_error(data_riders[:a], avg_riders[:a])) + np.sqrt(mean_squared_error(data_drivers[:a], avg_drivers[:a]))
+
             # Register score
             scores.append(mu_r + mu_d + rmse)
             if best is None:
                 best = np.mean(rmse) + 1
             newBest = min(best, np.mean(rmse))
             if newBest != best:
-                with open(f'half_pop_fit/rmse_output_fixed{args.fixed}.txt', 'a') as checkpoints:
+                with open(f'70_pop_fit/rmse_output_fixed{args.fixed}.txt', 'a') as checkpoints:
                     checkpoints.write(f'New best: mu_r: {mu_r} | mu_d: {mu_d}\nRMSE: {rmse}\n\n')
                 best = newBest
         except Exception as e:
-            with open(f'half_pop_fit/rmse_output_fixed{args.fixed}.txt', 'a') as checkpoints:
+            with open(f'70_pop_fit/rmse_output_fixed{args.fixed}.txt', 'a') as checkpoints:
                 checkpoints.write(f'ERROR\nmu_r: {mu_r} | mu_d: {mu_d}\n{e}\n\n')
 
-    np.savetxt(f'half_pop_fit/rmse_results_fixed{args.fixed}.txt', scores)
+    np.savetxt(f'70_pop_fit/rmse_results_fixed{args.fixed}.txt', scores)
 
 
 
