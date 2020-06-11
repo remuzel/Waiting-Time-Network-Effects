2685ab0941101f593dcd2b0c3cb23a72ffd7fd3f
Remi <rku16@ic.ac.uk>
2020-06-11 13:27:18 +0100

    finding optimal fit for dual platform

--- code/agent_simulation/curve_fit.py
+++ code/agent_simulation/curve_fit.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     parser.add_argument('--fixed', help='Which of the mu_waiting parameters to fix', type=int, default=0)
     args = parser.parse_args()
 
-    names = ['Uber', 'Lyft', 'Other']
+    names = ['Uber', 'Other']
 
     # Extrapolate from given market-share values
     N = 1889
@@ -34,7 +34,11 @@ if __name__ == "__main__":
         midpoint_interpolation(data[1], N-95)[65:] + [data[1,-1]],
         midpoint_interpolation(data[2], N-95)[65:] + [data[2,-1]]
     ]
-
+    data_ms = [
+        np.array(data_ms[0]),
+        np.array(data_ms[1]) + np.array(data_ms[2]),
+    ]
+    
     # Define the parameters to search for
     mu_rs = np.linspace(0, 1, num=args.mu_waiting)
     mu_rs = np.delete(mu_rs, [0, args.mu_waiting-1])
@@ -42,8 +46,11 @@ if __name__ == "__main__":
     mu_ds = np.delete(mu_ds, [0, args.mu_idle-1])
 
     # Assert that for each platform, the average of mu_waiting and mu_idle is not 'high'
-    valid = lambda elements: all([np.mean([elements[i], elements[i+3]]) <= 0.6 for i in [0, 1, 2]])
-    parameters = [[[a, b, c], [d, e, f]] for a in mu_rs for b in mu_rs for c in mu_rs for d in mu_ds for e in mu_ds for f in mu_ds if valid([a, b, c, d, e, f])]
+    # valid = lambda elements: all([np.mean([elements[i], elements[i+3]]) <= 0.6 for i in [0, 1, 2]])
+    valid = lambda elements: all([np.mean([elements[i], elements[i+2]]) <= 0.6 for i in [0, 1]])
+    # parameters = [[[a, b, c], [d, e, f]] for a in mu_rs for b in mu_rs for c in mu_rs for d in mu_ds for e in mu_ds for f in mu_ds if valid([a, b, c, d, e, f])]
+    parameters = [[[a, b], [c, d]] for a in mu_rs for b in mu_rs for c in mu_ds for d in mu_ds if valid([a, b, c, d])]
+
     # Perform grid search
     scores = []
     best = 1
@@ -56,8 +63,8 @@ if __name__ == "__main__":
             # Run the simulation it times
             for _ in (range(args.it)):
                 sim = AgentSimulator(N, names, rider_proportion=0.95,
-                                    mu_D=mu_d, mu_R=mu_r, eta=[0, 0, 0],
-                                    n_joins=1, delays=[0, 65, 65])
+                                    mu_D=mu_d, mu_R=mu_r, eta=[0, 0],
+                                    n_joins=1, delays=[0, 65])
                 # Store the returned data
                 iter_ms.append(sim.run().get_market_shares())
             # Get means of winner / looser over the runs
@@ -74,14 +81,14 @@ if __name__ == "__main__":
             scores.append(mu_r + mu_d + rmse)
             newBest = min(best, np.mean(rmse))
             if newBest != best:
-                with open(f'full_conditional_txtoutput/rmse_output_fixed{args.fixed}.txt', 'a') as checkpoints:
+                with open(f'dual_full_conditional_txtoutput/rmse_output_fixed{args.fixed}.txt', 'a') as checkpoints:
                     checkpoints.write(f'New best: mu_r: {mu_r} | mu_d: {mu_d}\nRMSE: {rmse}\n\n')
                 best = newBest
         except Exception as e:
-            with open(f'full_conditional_txtoutput/rmse_output_fixed{args.fixed}.txt', 'a') as checkpoints:
+            with open(f'dual_full_conditional_txtoutput/rmse_output_fixed{args.fixed}.txt', 'a') as checkpoints:
                 checkpoints.write(f'ERROR\nmu_r: {mu_r} | mu_d: {mu_d}\n{e}\n\n')
 
-    np.savetxt(f'full_conditional_txtoutput/rmse_results_fixed{args.fixed}.txt', scores)
+    np.savetxt(f'dual_full_conditional_txtoutput/rmse_results_fixed{args.fixed}.txt', scores)
 
 
 
